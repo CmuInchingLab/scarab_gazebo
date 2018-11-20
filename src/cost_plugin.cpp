04eaab14960ce531362d9293a2ab841129e37702
andrewww2 <awong2@andrew.cmu.edu>
2018-11-19 21:19:33 -0500

    separated worldplugin from ROS node

--- src/cost_plugin.cpp
+++ src/cost_plugin.cpp
@@ -4,7 +4,6 @@
 #include <ignition/math/Vector3.hh>
 #include <gazebo/math/Vector2i.hh>
 // http://docs.ros.org/jade/api/gazebo_msgs/html/msg/ModelState.html
-#include <gazebo_msgs/ModelStates.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Twist.h>
@@ -59,37 +58,4 @@ double heightMapZ(double x, double y){
 
 };
 GZ_REGISTER_WORLD_PLUGIN(CostPlugin);
-}
-using namespace std;
-// Get current X and Y coordinates of scarab robot. 
-// This is for testing whether we can get Z coordinates given X and Y
-void pose_cb(const gazebo_msgs::ModelStates& gazebo_msg){ 
-    // scarab robot is 2nd element in model position array
-    geometry_msgs::Point position = gazebo_msg.pose[1].position;
-    cout << "In callback. X:" << position.x << " Y:" << position.y << endl;
-
-}
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "main_scarab");
-  ros::NodeHandle n;
-//   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("cost", 1000);
-  ros::Subscriber sub = n.subscribe("/gazebo/model_states", 100, pose_cb);
-  ros::Rate loop_rate(10);
-
-  int count = 0;
-
-
-  while (ros::ok())
-  {
-    // chatter_pub.publish(msg);
-
-    ros::spinOnce();
-    loop_rate.sleep();
-    // ++count;
-  }
-
-
-  return 0;
 }
