c866a7c68e3e0b6833f25ba936b28fce3f22a395
andrewww2 <awong2@andrew.cmu.edu>
2018-11-21 02:17:16 -0500

    Getting Z values works!

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # scarab_gazebo
-Gazebo simulation files for Scarab robot in [Victoria Crater](https://nasa3d.arc.nasa.gov/detail/marstrek-victoriacrater).
+Gazebo simulation files for Scarab robot in some random place in the Rocky Mountains. Yay Colorado! 
 
-To run the simulation:
+To run the simulation for the Rocky Mountain environment:
 ```
-roslaunch scarab_gazebo scarab_world_victoria.launch
+roslaunch scarab_gazebo rocky_world.launch
 ```
+TODO:
+1) Match IDs between request for z value, and reply
+2) Ask teammates if querying series of Points is suitable for getSucessors()
--- src/cost_plugin.cc
+++ src/cost_plugin.cc
@@ -1,14 +1,16 @@
 #include <thread>
 #include <gazebo/common/Plugin.hh>
 #include <ros/ros.h>
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
 #include <gazebo/physics/physics.hh>
 #include <ignition/math/Vector3.hh>
 #include <gazebo/math/Vector2i.hh>
-// http://docs.ros.org/jade/api/gazebo_msgs/html/msg/ModelState.html
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/Twist.h>
-#include "scarab_gazebo/getZ.h"
+#include <geometry_msgs/Point.h>
+#include <scarab_gazebo/PointArr.h>
 
 using namespace std;
 namespace gazebo
@@ -21,7 +23,13 @@ private:
   physics::HeightmapShapePtr heightmap = NULL;
   physics::CollisionPtr collision = NULL;
   ros::ServiceServer service;
-  unique_ptr<ros::NodeHandle> rosNode;
+  unique_ptr<ros::NodeHandle> rosNode;  // A node use for ROS transport
+  ros::CallbackQueue rosQueue;  // A ROS callbackqueue that helps process messages
+  thread rosQueueThread; //A thread the keeps running the rosQueue
+  ros::Subscriber req_z_sub;
+  ros::Publisher send_z_pub;
+  math::Vector3 size;
+  math::Vector2i vc;
 
 public:
   CostPlugin() : WorldPlugin(){}
@@ -39,34 +47,65 @@ public:
       return;
     }
 
-    // Create our ROS node. This acts in a similar manner to
-    // the Gazebo node
+    // service = rosNode->advertiseService("getZ", &CostPlugin::heightMapZ, this);
+    // http://gazebosim.org/tutorials?tut=guided_i6
+    // Create our ROS node. This acts in a similar manner to the Gazebo node
     this->rosNode.reset(new ros::NodeHandle("scarab_gazebo"));
-    service = rosNode->advertiseService("getZ", &CostPlugin::heightMapZ, this);
+
+    //set subscriber options and subscribe to "request_z" topic
+    ros::SubscribeOptions so = ros::SubscribeOptions::create<scarab_gazebo::PointArr>(
+      "/scarab_gazebo/request_z", 1, boost::bind(&CostPlugin::heightMapZ, this, _1), ros::VoidPtr(), &this->rosQueue);
+
+    this->req_z_sub = this->rosNode->subscribe(so);
+    send_z_pub = rosNode->advertise< scarab_gazebo::PointArr>("/scarab_gazebo/response_z", 10);
+    // Spin up the queue helper thread.
+    this->rosQueueThread = std::thread(std::bind(&CostPlugin::QueueThread, this));
     ROS_INFO("WORLD PLUGIN INITIALIZED. YEA BOIII");
   }
 
-  bool heightMapZ(scarab_gazebo::getZ::Request &req, scarab_gazebo::getZ::Response &res)
+  // return z value at given x and y coordaintes. 
+  // ROS/Gazebo are great, aren't they?
+  void heightMapZ(const scarab_gazebo::PointArrConstPtr& pointArr)
   {
     if (world_ptr == NULL)
     { // only initialize once
       // getting the pointer to the HeightmapShape
       world_ptr = physics::get_world();
-      model = world_ptr->GetModel("Victoria_Crater");
-      // physics::CollisionPtr collision = model->GetLink("victoria_crater_link")->GetCollision("victoria_crater_collision");
-      // heightmap = boost::dynamic_pointer_cast<physics::HeightmapShape>(collision->GetShape());
+      model = world_ptr->GetModel("heightmap");
+      physics::CollisionPtr collision = model->GetLink("link")->GetCollision("collision");
+      heightmap = boost::dynamic_pointer_cast<physics::HeightmapShape>(collision->GetShape());
+      this->size = this->heightmap->GetSize();
+      this->vc = this->heightmap->GetVertexCount();
     }
 
-    // // coordinate transform from regular Word (x,y) to the HeightmapShape (index_x,index_y)
-    // math::Vector3 size = this->heightmap->GetSize();
-    // math::Vector2i vc = this->heightmap->GetVertexCount();
-    // int index_x = (((req.x + size.x / 2) / size.x) * vc.x - 1);
-    // int index_y = (((-req.y + size.y / 2) / size.y) * vc.y - 1);
+    if (pointArr->points.size() > 0){
+      scarab_gazebo::PointArr resp;
+      resp.id = pointArr->id;
+      for (int i = 0; i < pointArr->points.size(); i++){
+        float x = pointArr->points[i].x;
+        float y = pointArr->points[i].y;
+        geometry_msgs::Point curr_pt;
+        curr_pt.x = x;
+        curr_pt.y = y;
+        
+        // coordinate transform from regular Word (x,y) to the HeightmapShape (index_x,index_y)
+        int index_x = (((x + size.x / 2) / size.x) * vc.x - 1);
+        int index_y = (((-y + size.y / 2) / size.y) * vc.y - 1);
+        curr_pt.z = this->heightmap->GetHeight(index_x, index_y);  //  getting the height
+        resp.points.push_back(curr_pt);
+      }
+      send_z_pub.publish(resp);
+      }
+  }
 
-    // //  getting the height
-    // res.z = this->heightmap->GetHeight(index_x, index_y);
-    res.z = 0;
-    return true;
+  // ROS helper function that processes messages
+  private: void QueueThread()
+  {
+    static const double timeout = 0.01;
+    while (this->rosNode->ok())
+    {
+      this->rosQueue.callAvailable(ros::WallDuration(timeout));
+    }
   }
 };
 GZ_REGISTER_WORLD_PLUGIN(CostPlugin);
